8a1f998b07d538516bbf8308f33f847d84092087
Max Charles Jr <mcharles1682@gmail.com>
2019-05-08 06:13:33 -0400

    remove '.btn' for smooth scrolling

--- 04_EdgeLedger/js/main.js
+++ 04_EdgeLedger/js/main.js
@@ -9,37 +9,38 @@
 
 // Initialize and add map
 function initMap() {
-    // Your location
-    const loc = { lat: 42.361145, lng: -71.057083 };
-    // Centered map on location
-    const map = new google.maps.Map(document.querySelector('.map'), {
-        center: loc,
-        zoom: 14
-    });
-    // The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
+  // Your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    center: loc,
+    zoom: 14
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
 // Sticky menu background
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 150) {
-        document.querySelector('#navbar').getElementsByClassName.opacity = 0.9;
-    } else {
-        document.querySelector('#navbar').getElementsByClassName.opacity = 1;
-    }
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").getElementsByClassName.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").getElementsByClassName.opacity = 1;
+  }
 });
 
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+$("#navbar a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $('html, body').animate({
-                scrollTop: $(hash).offset().top - 65
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      800
+    );
+  }
 });
